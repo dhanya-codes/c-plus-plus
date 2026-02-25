bdfe18ce186c5959f690223a9d7e58c189226f62
dhanya-codes <2401107021@ptuniv.edu.in>
2026-02-24 22:01:11 -0800

    NEW version of scalar

--- scalar code using template.cpp
+++ scalar code using template.cpp
@@ -1,55 +1,49 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-template<class T>
-class vector
+template <class T>
+class scalar
 {
-    T v1[100], v2[100];
-    int n;
-
-public:
-    void input()
-    {
-        cout<<"Enter number of elements: ";
-        cout<<"Enter elements of first vector:\n";
-        for(int i=0;i<n;i++)
-        {
-            cin>>v1[i];
-        }
-
-        cout<<"Enter elements of second vector:\n";
-        for(int i=0;i<n;i++)
-        {
-            cin>>v2[i];
-        }
-    }
-
-    T scalarProduct()
-    {
-        T sum =0;
-        for (int i=0;i<n;i++)
-        {
-            sum+= v1[i]*v2[i];
-        }
-        return sum;
-    }
-
-    void display()
-    {
-        cout<<"Scalar Product ="<<scalarProduct()<<endl;
-    }
+	public:
+	T v1[100],v2[100];
+	int n;
+	public:
+		void getdata()
+		{
+			cout<<"Enter the number of digits:";
+			cin>>n;
+			cout<<"Enter the value 1:";
+			for(int i=0;i<n;i++)
+			{
+				cin>>v1[i];
+			}
+			cout<<"Enter the value 2:";
+			for(int i=0;i<n;i++)
+			{
+				cin>>v2[i];
+			}
+		}
+		T sum()
+		{
+			T sum=0;
+			for(int i=0;i<n;i++)
+			{
+				sum+=v1[i]*v2[i];
+			}
+			return sum;
+		}
+		void display()
+		{
+			cout<<"The vector sum"<<sum()<<endl;
+		}
+		
 };
-
 int main()
 {
-    cout << "For Integer Vectors";
-    vector<int> intr;//space should be there between objs
-    intr.input();
-    intr.display();
-
-    cout << "\nFor Float Vectors";
-    vector<float> floatr;
-    floatr.input();
-    floatr.display();
-
-    return 0;
+	scalar<int> ints;
+	ints.getdata();
+	ints.display();
+	scalar<float> floats;
+	floats.getdata();
+	floats.display();
+	
 }
